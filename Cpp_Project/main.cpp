eb3f0f3a773581ecd92a01202d9b37e03b17cc41
CoderXL1 <leo_xia@icloud.com>
2025-09-18 18:44:57 +0800

    Uploaded Cpp_Project

---
+++ Cpp_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Created by Leo Xia
+//  Created with VSCode on Ubuntu 22.04
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello, DXRMV!\n";
+    return 0;
+}
